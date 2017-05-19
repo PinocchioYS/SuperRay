74378f46f5bb59c6a7ce3bb9615a4825e05de1c1
PinocchioYS <tea984811@gmail.com>
2017-05-19 15:16:25 +0900

    Refine variable name: tree -> grid

--- gridmap3D/src/example_superrayGrid3D.cpp
+++ gridmap3D/src/example_superrayGrid3D.cpp
@@ -7,12 +7,12 @@
 void printUsage(char* self){
 	std::cout << "USAGE: " << self << " [options]" << std::endl << std::endl;
 	std::cout << "This tool inserts the data of a scan graph file (point clouds with poses)" << std::endl;
-	std::cout << "into an octree using Super Ray based Updates method." << std::endl;
-	std::cout << "The output is a compact maximum-likelihood binary octree file(.bt, bonsai tree)." << std::endl;
+	std::cout << "into a grid in 3D using Super Ray based Updates method." << std::endl;
+	std::cout << "The output is a compact maximum-likelihood binary grid3D file(.bg3, bonsai tree)." << std::endl;
 
 	std::cout << "Options: " << std::endl;
 	std::cout << " -i <InputFile.graph> (required)" << std::endl;
-	std::cout << " -o <OutputFile.bt> (required)" << std::endl;
+	std::cout << " -o <OutputFile.bg3> (required)" << std::endl;
 	std::cout << " -res <resolution[m]> (optional, default 0.1m)" << std::endl;
 	std::cout << " -thr <threshold> (optional, default 20)" << std::endl;
 
@@ -75,8 +75,8 @@ int main(int argc, char** argv) {
 		(*scan_it)->pose = gridmap3D::pose6d(transformed_sensor_origin, gridmath3D::Quaternion());
 	}
 
-	std::cout << "\nCreating tree\n===========================\n";
-	gridmap3D::SuperRayGrid3D* tree = new gridmap3D::SuperRayGrid3D(res);
+	std::cout << "\nCreating grid\n===========================\n";
+	gridmap3D::SuperRayGrid3D* grid = new gridmap3D::SuperRayGrid3D(res);
 
 	double time_to_update = 0.0;	// sec
 	size_t currentScan = 1;
@@ -85,7 +85,7 @@ int main(int argc, char** argv) {
 
 		// Generate Super Ray
 		gettimeofday(&start, NULL);  // start timer
-		tree->insertSuperRayCloudRays((*scan_it)->scan, (*scan_it)->pose.trans(), threshold);
+		grid->insertSuperRayCloudRays((*scan_it)->scan, (*scan_it)->pose.trans(), threshold);
 		gettimeofday(&stop, NULL);  // stop timer
 		time_to_update += (stop.tv_sec - start.tv_sec) + 1.0e-6 * (stop.tv_usec - start.tv_usec);;
 
@@ -96,10 +96,10 @@ int main(int argc, char** argv) {
 	std::cout << "Time to insert scans (SR): " << time_to_update << " [sec]" << std::endl;
 	std::cout << "Time to insert 100.000 points took (SR): " << time_to_update / ((double)graph->getNumPoints() / 100000) << " [sec] (avg)" << std::endl << std::endl;
 
-//	tree->writeBinary(treeFilename);
+	grid->writeBinary(treeFilename);
 
 	delete graph;
-	delete tree;
+	delete grid;
 
 	return 0;
 }
