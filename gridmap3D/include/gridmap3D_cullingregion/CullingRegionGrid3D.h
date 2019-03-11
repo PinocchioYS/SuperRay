e3fd39983fa72de734d3042fffa42f5c792e19ca
yskwon <youngsun.kwon@kaist.ac.kr>
2019-03-11 22:50:12 +0900

    Add the culling region based updates

---
+++ gridmap3D/include/gridmap3D_cullingregion/CullingRegionGrid3D.h
@@ -0,0 +1,150 @@
+/*
+* Copyright(c) 2019, Youngsun Kwon, Donghyuk Kim, Inkyu An, and Sung-eui Yoon, KAIST
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met :
+*
+*     * Redistributions of source code must retain the above copyright notice, this
+*       list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright notice,
+*       this list of conditions and the following disclaimer in the documentation
+*       and / or other materials provided with the distribution.
+*     * Neither the name of SuperRay nor the names of its
+*       contributors may be used to endorse or promote products derived from
+*       this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
+#ifndef GRIDMAP3D_CULLINGREGION_GRID3D_H
+#define GRIDMAP3D_CULLINGREGION_GRID3D_H
+
+#include <gridmap3D/gridmap3D.h>
+#include <gridmap3D_superray/SuperRayGenerator.h>
+
+namespace gridmap3D{
+    class CullingRegionGrid3D : public OccupancyGrid3DBase<Grid3DNode> {
+    public:
+        /// Default constructor, sets resolution of grid
+        CullingRegionGrid3D(double resolution);
+
+        /**
+         * Reads a Grid3D from a binary file
+         * @param _filename
+         *
+         */
+        // CullingRegionGrid3D(std::string _filename);
+
+        virtual ~CullingRegionGrid3D(){};
+
+        /// virtual constructor: creates a new object of same type
+        /// (Covariant return type requires an up-to-date compiler)
+        CullingRegionGrid3D* create() const { return new CullingRegionGrid3D(resolution); }
+
+        std::string getGridType() const { return "CullingRegionGrid3D"; }
+
+        // Super Rays and Culling Region based Updates
+
+        /**
+		 * Integrate a Pointcloud (in global reference frame), parallelized with OpenMP.
+		 * This function simply inserts all rays of the point clouds, similar to insertPointCloudRays of gridmap3D::Grid3D.
+		 * Occupied nodes have a preference over free ones.
+		 *
+		 * @param scan Pointcloud (measurement endpoints), in global reference frame
+		 * @param origin measurement origin in global reference frame
+		 */
+        virtual void insertPointCloudRays(const Pointcloud& scan, const point3d& origin);
+
+        /**
+		 * Integrate a Pointcloud (in global reference frame) using SuperRay, parallelized with OpenMP.
+		 * This function converts a point clouds into superrays, and then inserts all superrays out of the point clouds.
+		 * Occupied nodes have a preference over free ones.
+		 *
+		 * @param scan Pointcloud (measurement endpoints), in global reference frame
+		 * @param origin measurement origin in global reference frame
+		 * @param threshold threshold for limiting to generate super rays
+		 */
+        virtual void insertSuperRayCloudRays(const Pointcloud& scan, const point3d& origin, const int threshold);
+
+    protected:
+        /**
+		 * Build a culling region by utilizing the occupancy information updated to the map.
+		 * The implementation is based on a priority queue according to the Manhattan distance from the origin cell.
+         *
+		 * @param origin measurement origin in global reference frame
+		 * @param max_propagation maximum level of propagation; Manhattan distance from the origin cell
+         * @return culling region limited by the maximum level of the propagation
+		 */
+        KeySet buildCullingRegion(const point3d& origin, const int max_propagation);
+
+        /**
+		 * Build a culling region by utilizing the occupancy information updated to the map.
+         *
+         * @param scan Pointcloud (measurement endpoints), in global reference frame
+		 * @param origin measurement origin in global reference frame
+         * @return culling region limited by the range of measurements
+		 */
+        KeySet buildCullingRegion(const Pointcloud& scan, const point3d& origin);
+
+        /**
+		 * Build a culling region by utilizing the occupancy information updated to the map.
+         *
+         * @param superrays Super rays computed from the measurements in global reference frame
+		 * @param origin measurement origin in global reference frame
+         * @return culling region limited by the range of measurements
+		 */
+        KeySet buildCullingRegion(const SuperRayCloud& superrays, const point3d& origin);
+
+        /**
+         * Traces a sensor ray from origin (excluding) to end in the inverse direction (see the description),
+         * returning Grid3DKeys of all nodes traversed by the beam.
+         * During the traversal, the culling region stops the traversal when the ray
+         * encounters the region for reducing the unnecessary traversals and updates.
+         *
+         * @param origin start coordinate of ray (end point of sensor ray)
+         * @param end end coordinate of ray (sensor origin)
+         * @param ray KeyRay structure that holds the keys of all nodes traversed by the ray, excluding the origin cell
+         * @return Success of operation. Returning false usually means that one of the coordinates is out of the Grid3D's range
+         */
+        bool computeInverseRayKeys(const point3d& origin, const point3d& end, KeyRay& ray, KeySet& cullingregion);
+
+
+        /**
+         * Static member object which ensures that this Grid3D's prototype
+         * ends up in the classIDMapping only once. You need this as a
+         * static member in any derived grid3D class in order to read .ot
+         * files through the AbstractGrid3D factory. You should also call
+         * ensureLinking() once from the constructor.
+         */
+        class StaticMemberInitializer{
+        public:
+            StaticMemberInitializer() {
+                CullingRegionGrid3D* grid = new CullingRegionGrid3D(0.1);
+                grid->clearKeyRays();
+                AbstractGrid3D::registerGridType(grid);
+            }
+
+            /**
+             * Dummy function to ensure that MSVC does not drop the
+             * StaticMemberInitializer, causing this grid failing to register.
+             * Needs to be called from the constructor of this grid3D.
+             */
+            void ensureLinking() {};
+        };
+        /// static member to ensure static initialization (only once)
+        static StaticMemberInitializer cullingregionGrid3DMemberInit;
+    };
+}
+
+#endif
---
+++ gridmap3D/src/CullingRegionGrid3D.cpp
@@ -0,0 +1,337 @@
+/*
+* Copyright(c) 2019, Youngsun Kwon, Donghyuk Kim, Inkyu An, and Sung-eui Yoon, KAIST
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met :
+*
+*     * Redistributions of source code must retain the above copyright notice, this
+*       list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright notice,
+*       this list of conditions and the following disclaimer in the documentation
+*       and / or other materials provided with the distribution.
+*     * Neither the name of SuperRay nor the names of its
+*       contributors may be used to endorse or promote products derived from
+*       this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
+#include <gridmap3D_cullingregion/CullingRegionGrid3D.h>
+
+namespace gridmap3D{
+    CullingRegionGrid3D::CullingRegionGrid3D(double in_resolution)
+            : OccupancyGrid3DBase<Grid3DNode>(in_resolution) {
+        cullingregionGrid3DMemberInit.ensureLinking();
+    };
+
+    /*CullingRegionGrid3D::CullingRegionGrid3D(std::string _filename)
+            : OccupancyGrid3DBase<Grid3DNode>(0.1)  { // resolution will be set according to grid file
+        readBinary(_filename);
+    }*/
+
+    CullingRegionGrid3D::StaticMemberInitializer CullingRegionGrid3D::cullingregionGrid3DMemberInit;
+
+    void CullingRegionGrid3D::insertPointCloudRays(const Pointcloud& pc, const point3d& origin)
+    {
+        if (pc.size() < 1)
+            return;
+
+        // Build a culling region
+        KeySet cullingregion = buildCullingRegion(pc, origin);
+
+        // Update the occupancies of the map
+#ifdef _OPENMP
+        omp_set_num_threads(this->keyrays.size());
+#pragma omp parallel for
+#endif
+        for (int i = 0; i < (int)pc.size(); ++i) {
+            const point3d& p = pc[i];
+            unsigned threadIdx = 0;
+#ifdef _OPENMP
+            threadIdx = omp_get_thread_num();
+#endif
+            KeyRay* keyray = &(this->keyrays.at(threadIdx));
+
+            if (this->computeInverseRayKeys(p, origin, *keyray, cullingregion)){
+#ifdef _OPENMP
+#pragma omp critical
+#endif
+                {
+                    // Update the traversed cells to have the free states
+                    for (KeyRay::iterator it = keyray->begin(); it != keyray->end(); ++it){
+                        updateNode(*it, false);
+                    }
+                }
+            }
+        }
+
+        // Update the cells containing the end points to have the occupied states
+        for (int i = 0; i < (int)pc.size(); ++i){
+            updateNode(pc[i], true);
+        }
+    }
+
+    void CullingRegionGrid3D::insertSuperRayCloudRays(const Pointcloud& pc, const point3d& origin, const int threshold)
+    {
+        if (pc.size() < 1)
+            return;
+
+        // Generate the super rays
+        SuperRayGenerator srgenerator(resolution, grid_max_val, threshold);
+        SuperRayCloud srcloud;
+        srgenerator.GenerateSuperRay(pc, origin, srcloud);
+
+        // Build a culling region
+        KeySet cullingregion = buildCullingRegion(srcloud, origin);
+
+        // Update the occupancies of the map
+#ifdef _OPENMP
+        omp_set_num_threads(this->keyrays.size());
+#pragma omp parallel for
+#endif
+        for (int i = 0; i < (int)srcloud.size(); ++i) {
+            const point3d& p = srcloud[i].p;
+            unsigned threadIdx = 0;
+#ifdef _OPENMP
+            threadIdx = omp_get_thread_num();
+#endif
+            KeyRay* keyray = &(this->keyrays.at(threadIdx));
+
+            if (this->computeInverseRayKeys(p, origin, *keyray, cullingregion)){
+#ifdef _OPENMP
+#pragma omp critical
+#endif
+                {
+                    // Update the traversed cells to have the free states
+                    for (KeyRay::iterator it = keyray->begin(); it != keyray->end(); it++) {
+                        updateNode(*it, prob_miss_log * srcloud[i].w);
+                    }
+                }
+            }
+        }
+
+        // Update the cells containing the end points to have the occupied states
+        for (int i = 0; i < (int)srcloud.size(); ++i){
+            updateNode(srcloud[i].p, prob_hit_log * srcloud[i].w);
+        }
+    }
+
+    KeySet CullingRegionGrid3D::buildCullingRegion(const point3d& origin, const int max_propagation)
+    {
+        KeySet cullingregion;
+
+        Grid3DKey originKey = coordToKey(origin);
+
+        KeySet* cur_candidates = new KeySet;
+        cur_candidates->insert(originKey);
+
+        for(int cur_level = 0; cur_level <= max_propagation; cur_level++){
+            KeySet* next_candidates = new KeySet;
+            for(KeySet::iterator it = cur_candidates->begin(); it != cur_candidates->end(); it++){
+                // Check the insertion of the cell into the culling region
+                const Grid3DKey& key = *it;
+
+                // The first condition: does the cell have a fully free state?
+                int step[3] = {0, 0, 0};
+                Grid3DNode* node = search(key);
+                if (!node || node->getLogOdds() > clamping_thres_min)
+                    continue;
+
+                // The second condition: are all the neighbor cells in the culling region?
+                bool insertion = true;
+                for (int axis = 0; axis < 3; axis++){
+                    // Find a neighbor cell in the direction of the axis
+                    if (key[axis] > originKey[axis])		step[axis] = -1;
+                    else if (key[axis] < originKey[axis])	step[axis] = 1;
+
+                    if (step[axis] != 0){
+                        // Check a neighbor cell
+                        Grid3DKey checkKey = key;
+                        checkKey[axis] += step[axis];
+
+                        // The neighbor cell is not in culling region
+                        if (cullingregion.find(checkKey) == cullingregion.end()){
+                            insertion = false;
+                            break;
+                        }
+                    }
+                }
+
+                // Insert the cell into the culling region
+                if(!insertion)
+                    continue;
+                cullingregion.insert(key);
+
+                // Find the candidates in the next level
+                if(cur_level != max_propagation){
+                    for (int axis = 0; axis < 3; axis++){
+                        Grid3DKey candidate = key;
+
+                        if (step[axis] != 0){
+                            candidate[axis] += (-step[axis]);
+                            next_candidates->insert(candidate);
+                        }
+                        else{
+                            candidate[axis] += 1;
+                            next_candidates->insert(candidate);
+
+                            candidate[axis] += -2;
+                            next_candidates->insert(candidate);
+                        }
+                    }
+                }
+            }
+
+            // Propagation: move to the next level
+            delete cur_candidates;
+            cur_candidates = next_candidates;
+            if(cur_candidates->size() <= 0)
+                break;
+        }
+
+        delete cur_candidates;
+
+        return cullingregion;
+    }
+
+    KeySet CullingRegionGrid3D::buildCullingRegion(const Pointcloud& pc, const point3d& origin)
+    {
+        // Find the maximum distance between the sensor origin and the end point
+        double max_dist = 0.0;
+        for(int i = 0; i < (int)pc.size(); i++){
+            const point3d& p = pc[i];
+            double distance = (p - origin).norm();
+            if(distance > max_dist)
+                max_dist = distance;
+        }
+
+        // Build a culling region limited the minimum distance
+        return buildCullingRegion(origin, (int)(max_dist / resolution));
+    }
+
+    KeySet CullingRegionGrid3D::buildCullingRegion(const SuperRayCloud& superrays, const point3d& origin)
+    {
+        // Find the maximum distance between the sensor origin and the end point
+        double max_dist = 0.0;
+        for(int i = 0; i < (int)superrays.size(); i++){
+            const point3d& p = superrays[i].p;
+            double distance = (p - origin).norm();
+            if(distance > max_dist)
+                max_dist = distance;
+        }
+
+        // Build a culling region limited the minimum distance
+        return buildCullingRegion(origin, (int)(max_dist / resolution));
+    }
+
+    bool CullingRegionGrid3D::computeInverseRayKeys(const point3d& origin, const point3d& end, KeyRay& ray, KeySet& cullingregion)
+    {
+        // see "A Faster Voxel Traversal Algorithm for Ray Tracing" by Amanatides & Woo
+        // basically: DDA in 3D
+
+        ray.reset();
+
+        Grid3DKey key_origin, key_end;
+        if ( !coordToKeyChecked(origin, key_origin) || !coordToKeyChecked(end, key_end) ) {
+            GRIDMAP3D_WARNING_STR("coordinates ( " << origin << " -> " << end << ") out of bounds in computeRayKeys");
+            return false;
+        }
+
+        if (key_origin == key_end)
+            return true; // same tree cell, we're done.
+
+        // Initialization phase -------------------------------------------------------
+        point3d direction = (end - origin);
+        float length = (float) direction.norm();
+        direction /= length; // normalize vector
+
+        int    step[3];
+        double tMax[3];
+        double tDelta[3];
+
+        Grid3DKey current_key = key_origin;
+
+        for(unsigned int i = 0; i < 3; ++i) {
+            // compute step direction
+            if (direction(i) > 0.0)         step[i] = 1;
+            else if (direction(i) < 0.0)    step[i] = -1;
+            else                            step[i] = 0;
+
+            // compute tMax, tDelta
+            if (step[i] != 0) {
+                // corner point of voxel (in direction of ray)
+                double voxelBorder = this->keyToCoord(current_key[i]);
+                voxelBorder += (float) (step[i] * this->resolution * 0.5);
+
+                tMax[i] = ( voxelBorder - origin(i) ) / direction(i);
+                tDelta[i] = this->resolution / fabs( direction(i) );
+            }
+            else {
+                tMax[i] =  std::numeric_limits<double>::max( );
+                tDelta[i] = std::numeric_limits<double>::max( );
+            }
+        }
+
+        // Incremental phase  ---------------------------------------------------------
+        bool done = false;
+        while (!done) {
+            // find minimum tMax:
+            unsigned int dim;
+            if (tMax[0] < tMax[1]){
+                if (tMax[0] < tMax[2]) dim = 0;
+                else                   dim = 2;
+            }
+            else {
+                if (tMax[1] < tMax[2]) dim = 1;
+                else                   dim = 2;
+            }
+
+            // advance in direction "dim"
+            current_key[dim] += step[dim];
+            tMax[dim] += tDelta[dim];
+
+            assert (current_key[dim] < 2*this->grid_max_val);
+
+            // Culling out the traversal
+            if (cullingregion.find(current_key) != cullingregion.end()){
+                done = true;
+                break;
+            }
+            // reached endpoint, key equv?
+            else if (current_key == key_end) {
+                ray.addKey(current_key);
+                done = true;
+                break;
+            }
+            else {
+                // reached endpoint world coords?
+                // dist_from_origin now contains the length of the ray when traveled until the border of the current voxel
+                double dist_from_origin = std::min(std::min(tMax[0], tMax[1]), tMax[2]);
+                // if this is longer than the expected ray length, we should have already hit the voxel containing the end point with the code above (key_end).
+                // However, we did not hit it due to accumulating discretization errors, so this is the point here to stop the ray as we would never reach the voxel key_end
+                if (dist_from_origin > length) {
+                    done = true;
+                    break;
+                }
+                else {  // continue to add freespace cells
+                    ray.addKey(current_key);
+                }
+            }
+
+            assert ( ray.size() < ray.sizeMax() - 1);
+        } // end while
+
+        return true;
+    }
+}
